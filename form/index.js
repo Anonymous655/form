0f579dab250998bc9a31c4a5a5cd3b0cef929de3
Anonymous655 <55239017+Anonymous655@users.noreply.github.com>
2021-04-10 13:43:15 +0530

    added localStorage feature

--- form/index.js
+++ form/index.js
@@ -5,17 +5,27 @@ const email = document.getElementById('email');
 const btn = document.querySelector('.btn');
 
 const myForm = document.getElementById('my-form');
+const h33 = document.querySelector(".h33");
+const h34 = document.querySelector(".h34");
 
 myForm.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log('Name : ' + name.value)
     console.log('Email : ' + email.value)
-
+    
+    
+    
 } )
 
 btn.addEventListener('click', (e) => {
     e.preventDefault()
     btn.style.color = 'red'
+    localStorage.setItem('name', name.value)
+    localStorage.setItem('email', email.value)
+    h33.textContent = localStorage.getItem('name')
+    h34.textContent = localStorage.getItem('email')
+    
+    // console.log(h33)
 })
 btn.addEventListener('mouseover', (e) => {
     e.preventDefault()
@@ -25,3 +35,7 @@ btn.addEventListener('mouseout', (e) => {
     e.preventDefault()
     btn.style.color = 'yellow'
 })
+
+
+
+
